c64a95e99644a0d248e253666d70f3c91665022f
Hector Monsivais <hector.m.monsivais@gmail.com>
2022-10-22 17:07:26 -0500

    pointers

--- structs/main.go
+++ structs/main.go
@@ -10,22 +10,56 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	// using another struct
+	// embedding structs
+	// using a struct within another
 	contact contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (personPointer *person) updateName2(newFirstName string) {
+	(*personPointer).firstName = newFirstName
 }
 
 func main() {
 	// var alex person
-	//
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "mx@aol.com",
+			zipCode: 12345,
+		},
+	}
+	//fmt.Println(alex)
+	fmt.Printf("%+v\n", alex)
 
 	alex.firstName = "Alejandro"
 	alex.lastName = "Martinez"
 	alex.contact.email = "test@test.com"
 	alex.contact.zipCode = 75093
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	//fmt.Println(alex)
+	alex.print()
+
+	//This does not work
+	alex.updateName("Hector")
+	alex.print()
+
+	//Do this instead
+	personPointer := &alex
+	personPointer.updateName2("Hector")
+	alex.print()
+
+	//Short way...
+	//only works because the receiver (updateName2)
+	//is told to use the memory address
+	alex.updateName2("Victor")
+	alex.print()
 }
