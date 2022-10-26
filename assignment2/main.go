2560a416f3833644db71b6fb33ad737531d74897
Hector Monsivais <hector.m.monsivais@gmail.com>
2022-10-26 14:17:13 -0500

    assignments

---
+++ assignment2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func (t triangle) getArea() float64 {
+	return t.height * t.base * 0.5
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	sq := square{2}
+	tr := triangle{2, 2}
+	printArea(sq)
+	printArea(tr)
+}
